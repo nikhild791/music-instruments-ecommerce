8d37932ad8eae23814595257ee307bbbb698d2e3
nikhild791 <dubeynikhil791@gmail.com>
2025-03-13 09:15:20 +0530

    product updatation and ui changes

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -95,30 +95,54 @@ const Category = () => {
             "price":3444,
             "imgUrl":"https://music-club.mrsoni.me/uploads/Admin/ProductImage/Main/p_20-1.jpg"
         }]
-      
-    }
+    },
+    {
+        "categoryName":"PIANO",
+        "categoryImgUrl":"https://music-club.mrsoni.me/uploads/Admin/categoryImage/piano.jpg",
+        "categoryProduct":[]
+    },
+    {
+        "categoryName":"DRUM",
+        "categoryImgUrl":"https://music-club.mrsoni.me/uploads/Admin/categoryImage/drum.jpg",
+        "categoryProduct":[]
+    },
+    {
+        "categoryName":"FLUTE",
+        "categoryImgUrl":"https://music-club.mrsoni.me/uploads/Admin/categoryImage/flute.jpg",
+        "categoryProduct":[]
+    },
+    {
+        "categoryName":"MUSICIANs MALL",
+        "categoryImgUrl":"https://music-club.mrsoni.me/uploads/Admin/categoryImage/MUSICIANs_MALL.jpg",
+        "categoryProduct":[]
+    },
+    {
+        "categoryName":"SITAR",
+        "categoryImgUrl":"https://music-club.mrsoni.me/uploads/Admin/categoryImage/Sitar.jpg",
+        "categoryProduct":[]
+    },
     ]
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
 
-    <div className='flex flex-col justify-center items-center'>
+    <div className='flex flex-col  justify-center items-center'>
         <span className='text-3xl font-bold dark:text-white my-4 '>Category</span>
-        <div className='flex flex-row items-center gap-11 justify-center mb-12'>
+        <div className='flex flex-row flex-wrap items-center gap-11 justify-center mb-12'>
 
      
         {productArray.map((e)=>{
             return(<div>
 
         
-          <Card className="w-60 dark:bg-gray-900 dark:border-white border-black dark:border-2 rounded m-2">
-      <CardHeader shadow={false} floated={false} className="h-60">
+          <Card className="w-72 dark:bg-gray-900 dark:border-white  border-2 rounded m-2">
+      <CardHeader shadow={false} floated={false} className="h-60 mt-0">
         <img
           src={e.categoryImgUrl}
           className="h-full w-full object-cover"
         />
       </CardHeader>
       <CardBody>
-        <div className="mb-2 flex items-center justify-between text-center">
+        <div className="mb-2 flex items-center justify-center text-center">
           <Typography color="blue-gray" className="font-medium dark:text-white justify-center">
             {e.categoryName}
           </Typography>
